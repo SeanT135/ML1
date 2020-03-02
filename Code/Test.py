af780431b65a2de1151900d8d2831852fab68b3d
SeanT135 <seantemple1999@gmail.com>
2020-03-02 14:07:07 +0000

    Working SVM

--- Code/Test.py
+++ Code/Test.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
 
-import matplotlib
-import pandas
-import scikit_learn
+from matplotlib import *
+from pandas import *
+from sklearn import *
 
 csvData = pandas.read_csv("messidor_features.csv", sep=',', header=0, index_col=False, skipinitialspace=True)
-# Parameters: Filepath, Seperating Character, Header Row, Index Column (set to false, as no index column), Skip Initial Space (Ignores whitespace after seperationg character) 
+# Parameters: Filepath, Seperating Character, Header Row, Index Column (set to false, as no index column), Skip Initial Space (Ignores whitespace after seperationg character)
+
+csvDataX = csvData
+csvDataX.drop(index=19)
+csvDataY = csvData['Messidor Class']
+
+
+#print (csvData.size) 
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(csvDataX, csvDataY, test_size=0.25)
+
+#print (datasets[0].size) 
+#print (datasets[1].size)
+
+clf = svm.SVC(kernel='rbf')
+
+clf.fit(X_train, y_train)
+
+y_predict = clf.predict(X_test)
+
+print('Accuracy : ', metrics.accuracy_score(y_test, y_predict))
